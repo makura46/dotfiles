84436ffc4b129dcb0871125b62b2f784c7b8f9f7
makura46 <makura6706@gmail.com>
2019-08-22 04:29:32 +0900

    fix: recognized as a command

--- bin/setup.sh
+++ bin/setup.sh
@@ -15,18 +15,18 @@ setup () {
   cd $GIT_DIR
 
   # Install git
-  if [ ! type -p git > /dev/null ]; then
+  if [ ! $(type -p git) > /dev/null ]; then
     # macOS
-    if [ uname -a | fgrep -i Darwin > /dev/null ]; then
+	if [ $(uname -a) | $(fgrep -i Darwin) > /dev/null ]; then
       brew update
       brew install git
     # like debian
-    elif [ type -p apt > /dev/null ]; then
+    elif [ $(type -p apt) > /dev/null ]; then
       sudo apt autoremove -y
       sudo apt update -y
       sudo apt install -y git
     # CentOS
-    elif [ type -p yum > /dev/null ]; then
+    elif [ $(type -p yum) > /dev/null ]; then
       sudo yum clean -y
       sudo yum update -y
       sudo yum install -y git
