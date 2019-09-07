a4ce81eab29a4090383e7b828970929529e16143
makura46 <makura6706@gmail.com>
2019-09-07 11:54:48 +0900

    change: all ubuntu version

--- bin/install.sh
+++ bin/install.sh
@@ -19,23 +19,23 @@ _install_neovim () {
     brew install neovim
   # like debian
   elif type -p apt > /dev/null; then
-	if uname -a | fgrep -i "18.04" | fgrep -i "Ubuntu"; then
+	#if uname -a | fgrep -i "18.04" | fgrep -i "Ubuntu"; then
       sudo apt autoremove -y
       sudo apt update -y
       sudo apt install -y neovim
     # Ubuntu 18.04 than less
-    else
-      sudo apt-get autoremove -y
-      sudo apt-get install -y software-properties-common
-      sudo apt-get install -y python-software-properties
-      sudo add-apt-repository -y ppa:neovim-ppa/stable
-      sudo apt-get update -y
-      sudo apt-get install -y neovim
-      sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-      sudo apt-get install -y python-dev python-pip python3-dev
-      sudo apt-get install -y python3-setuptools
-      sudo easy_install3 -y pip
-    fi
+    #else
+    #  sudo apt-get autoremove -y
+    #  sudo apt-get install -y software-properties-common
+    #  sudo apt-get install -y python-software-properties
+    #  sudo add-apt-repository -y ppa:neovim-ppa/stable
+    #  sudo apt-get update -y
+    #  sudo apt-get install -y neovim
+    #  sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+    #  sudo apt-get install -y python-dev python-pip python3-dev
+    #  sudo apt-get install -y python3-setuptools
+    #  sudo easy_install3 -y pip
+    #fi
   # CentOS
   elif type -p yum > /dev/null; then
     sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
